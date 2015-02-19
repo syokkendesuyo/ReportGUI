4ec75826ae09f615c29d070c237eccd3b47124a2
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2015-02-19 23:01:50 +0900

    Update 0.0.1
    大体完成

--- src/main/java/net/jp/minecraft/plugin/ReportGUI.java
+++ src/main/java/net/jp/minecraft/plugin/ReportGUI.java
@@ -2,12 +2,11 @@ package net.jp.minecraft.plugin;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
@@ -21,7 +20,6 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.Metrics;
 
@@ -58,59 +56,30 @@ public class ReportGUI extends JavaPlugin implements Listener {
 	//コマンドで杖を渡す処理
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("ait")) {
+		if (cmd.getName().equalsIgnoreCase("report")) {
 			if (!(sender instanceof Player)) {
 				sender.sendMessage("Please excute this /ait command on a game!");
-				sender.sendMessage("/ait コマンドはゲーム内で実行してください。");
+				sender.sendMessage("/report コマンドはゲーム内で実行してください。");
 			}
 			else {
 				Player player = (Player) sender;
-				if(player.hasPermission("ait.give")||player.isOp()){
+				if(player.hasPermission("report.give")||player.isOp()){
 					//palyerがait.giveまたはopであれば杖を渡す
 
 					ItemStack item = new ItemStack(Material.STICK);
 					ItemMeta itemmeta = item.getItemMeta();
-					itemmeta.setDisplayName(ChatColor.GOLD + "AdminInventoryTools");
-					itemmeta.setLore(Arrays.asList(ChatColor.YELLOW + "魔法の杖:", ChatColor.WHITE + "この杖を空気に向かってクリックすると", ChatColor.WHITE + "スバラシイ画面が現れるだろう。"));
+					itemmeta.setDisplayName(ChatColor.GOLD + "ReportTool");
+					itemmeta.setLore(Arrays.asList(ChatColor.YELLOW + "魔法の杖:", ChatColor.WHITE + "この杖を空気に向かってクリックすると", ChatColor.WHITE + "通報画面が現れます。"));
 					itemmeta.addEnchant(Enchantment.SILK_TOUCH , 1, true);
 					item.setItemMeta(itemmeta);
 					player.getInventory().addItem(item);
 
-					player.sendMessage(ChatColor.AQUA + "[情報]AdminInventoryToolsを与えました。");
+					player.sendMessage(ChatColor.AQUA + "[情報]レポートツールを与えました。");
 				}
 			}
 			return true;
 		}
 
-		else if (cmd.getName().equalsIgnoreCase("skull")) {
-			if (!(sender instanceof Player)) {
-				sender.sendMessage("Please excute this /skull command on a game!");
-				sender.sendMessage("/skull コマンドはゲーム内で実行してください。");
-			}
-			else {
-				Player player = (Player) sender;
-				if(player.hasPermission("ait.skull")||player.isOp()){
-					//palyerがskull.giveまたはopであればここを抜ける
-					if(args.length == 0){
-						sender.sendMessage(ChatColor.AQUA + "[情報]/skull <player> でプレイヤーの頭を取得できます。");
-					}
-					else{
-					 ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1);
-					 SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-					 skull.setDurability((short) 3);
-					 skullMeta.setDisplayName(ChatColor.GOLD + args[0] + "の頭");
-					 skullMeta.setOwner(args[0]);
-					 skull.setItemMeta(skullMeta);
-
-					 skull.addUnsafeEnchantment(Enchantment.SILK_TOUCH, 1);
-					 player.getInventory().addItem(skull);
-
-					player.sendMessage(ChatColor.AQUA + "[情報]"+ args[0] + "の頭を与えました。");
-					}
-				}
-			}
-			return true;
-		}
 		return false;
 	}
 
@@ -118,14 +87,14 @@ public class ReportGUI extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onPlayerInteractEvent(PlayerInteractEvent event , ItemStack item , Player[] players){
 		final Player p = event.getPlayer();
-		if(p.hasPermission("ait.open")||p.isOp()){
+		if(p.hasPermission("report.open")||p.isOp()){
 			if(p.getItemInHand().getType()==Material.AIR){
 				//何でもなかった場合無視
 			}
 			else if(p.getItemInHand().getItemMeta().getDisplayName()==null){
 				//通常の棒だった場合無視する
 			}
-			else if(p.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "AdminInventoryTools")){
+			else if(p.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "ReportTool")){
 				if(event.getAction() == Action.LEFT_CLICK_AIR){
 					if(p.getItemInHand().getType() == Material.STICK){
 
@@ -166,90 +135,17 @@ public class ReportGUI extends JavaPlugin implements Listener {
 		if (event.getInventory().getName().equalsIgnoreCase("□Report□")){
 			if (event.getRawSlot() < 54 && event.getRawSlot() > -1){
 				Player player = (Player) event.getWhoClicked();
-				World world = player.getWorld();
 				if(event.isRightClick() || event.isLeftClick()|| event.getAction()==InventoryAction.HOTBAR_SWAP || event.getAction() == InventoryAction.HOTBAR_MOVE_AND_READD || event.getAction() == InventoryAction.DROP_ONE_SLOT ||event.getAction() == InventoryAction.DROP_ALL_SLOT){
-					if(event.getRawSlot()==0){
-						if(player.hasPermission("ait.gui.timeset.morning")){
-							world.setTime(0);
-							player.sendMessage(ChatColor.AQUA + "[情報]時間を0に設定しました。");
-							player.closeInventory();
-						}
-					}
 
-					if(event.getRawSlot()==1){
-						if(player.hasPermission("ait.gui.timeset.night")){
-							world.setTime(12500);
-							player.sendMessage(ChatColor.AQUA + "[情報]時間を12500に設定しました。");
-							player.closeInventory();
-						}
-					}
+					//loreからuuidを取得
+					ItemStack item = event.getCurrentItem();
+					ItemMeta meta = item.getItemMeta();
+					List<String> lore = meta.getLore();
+					String puuid =lore.get(0);
 
-					if(event.getRawSlot()==2){
-						if(player.hasPermission("ait.gui.gamemode")){
-							if(player.getGameMode() == GameMode.SURVIVAL){
-								player.setGameMode(GameMode.CREATIVE);
-								player.sendMessage(ChatColor.AQUA + "[情報]ゲームモードをクリエイティブにしました。");
-							}
-							else if(player.getGameMode() == GameMode.CREATIVE){
-								player.setGameMode(GameMode.SURVIVAL);
-								player.sendMessage(ChatColor.AQUA + "[情報]ゲームモードをサバイバルにしました。");
-							}
-							//サバイバル・クリエイティブでないゲームモードはあまり使わないよね？ってことで他はとりあえずサバイバルに設定
-							else{
-								player.setGameMode(GameMode.SURVIVAL);
-								player.sendMessage(ChatColor.AQUA + "[情報]ゲームモードをサバイバルにしました。");
-							}
-							player.closeInventory();
-						}
-					}
+					player.sendMessage(ChatColor.AQUA + "クリックしたPlayerのUUIDは" + puuid + "です。");
 
-					if(event.getRawSlot()==3){
-						if(player.hasPermission("ait.gui.whitelist")){
-							if(Bukkit.hasWhitelist()==true){
-								Bukkit.setWhitelist(false);
-								player.sendMessage(ChatColor.AQUA + "[情報]ホワイトリストを無効化しました。");
-							}
-							else if(Bukkit.hasWhitelist()==false){
-								Bukkit.setWhitelist(true);
-								player.sendMessage(ChatColor.AQUA + "[情報]ホワイトリストを有効化しました。");
-							}
-							player.closeInventory();
-						}
-					}
-
-					if(event.getRawSlot()==4){
-						if(player.hasPermission("ait.gui.skull")){
-							 ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1);
-							 SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-							 skull.setDurability((short) 3);
-							 skullMeta.setDisplayName(ChatColor.GOLD + player.getName() + "の頭");
-							 skullMeta.setOwner(player.getName());
-							 skull.setItemMeta(skullMeta);
-
-							 skull.addUnsafeEnchantment(Enchantment.SILK_TOUCH, 1);
-							 player.getInventory().addItem(skull);
-
-							 player.closeInventory();
-						}
-					}
-
-					if(event.getRawSlot()==5){
-						if(player.hasPermission("ait.gui.op")){
-							if(player.isOp()==true){
-								player.sendMessage(ChatColor.AQUA + "[情報]オペレータ権限を剥奪しました。");
-								player.setOp(false);
-							}
-							else if(player.isOp()==false){
-								player.sendMessage(ChatColor.AQUA + "[情報]オペレータ権限を取得しました。");
-								player.setOp(true);
-							}
-							player.closeInventory();
-						}
-					}
-
-					if(event.getRawSlot()==8){
-						player.closeInventory();
-					}
+					player.closeInventory();
 
 					event.setCancelled(true);
 				}
